bb48660c86a030ad71e86b77bebf05aab9e5ba22
Sturmjager13 <166233866+Sturmjager13@users.noreply.github.com>
2025-02-07 16:34:46 -0500

    Added Soap plugin

---
+++ Assets/Obvious/Soap/Core/Editor/Settings/SoapSettings.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Obvious.Soap.Editor
+{
+    public class SoapSettings : ScriptableObject
+    {
+        public EVariableDisplayMode VariableDisplayMode = EVariableDisplayMode.Default;
+        public ENamingCreationMode NamingOnCreationMode = ENamingCreationMode.Auto;
+        public ECreatePathMode CreatePathMode = ECreatePathMode.Auto;
+        public ERaiseEventInEditorMode RaiseEventsInEditor;
+        public bool CanEventsBeRaisedInEditor => RaiseEventsInEditor == ERaiseEventInEditorMode.Enabled;
+        [FormerlySerializedAs("Categories")] public List<string> Tags = new List<string> { "None" };
+
+        public int GetTagIndex(string tagName)
+        {
+            if (!Tags.Contains(tagName))
+            {
+                Debug.LogWarning($"Tag {tagName} does not exist. Returning 0.");
+                return 0;
+            }
+
+            return Tags.IndexOf(tagName);
+        }
+
+        public int GetTagIndex(int fromIndex)
+        {
+            if (fromIndex < 0 || fromIndex >= Tags.Count)
+            {
+                Debug.LogWarning($"Tag index {fromIndex} out of range. Returning 0.");
+                return 0;
+            }
+
+            return fromIndex;
+        }
+        
+        public int GetTagIndex(ScriptableBase scriptableBase)
+        {
+            var fromIndex = scriptableBase.TagIndex;
+            if (fromIndex < 0 || fromIndex >= Tags.Count)
+            {
+                Debug.Log($"Tag for {scriptableBase.name} not found. Reassigning to None.");
+                SoapEditorUtils.AssignTag(scriptableBase,0);
+                return 0;
+            }
+
+            return fromIndex;
+        }
+        
+        public string GetTagName(ScriptableBase scriptableBase)
+        {
+            var tagIndex = GetTagIndex(scriptableBase);
+            return Tags[tagIndex];
+        }
+    }
+
+    public enum EVariableDisplayMode
+    {
+        Default,
+        Minimal
+    }
+
+    public enum ENamingCreationMode
+    {
+        Auto,
+        Manual
+    }
+
+
+    public enum ECreatePathMode
+    {
+        Auto,
+        Manual
+    }
+
+    public enum ERaiseEventInEditorMode
+    {
+        Disabled,
+        Enabled
+    }
+}
